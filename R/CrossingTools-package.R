4bf09a13707da629a5b2bfb17870cda81bace214
svenernstW <Svenernst.weber@gmx.de>
2025-10-21 15:15:55 +0200

    Update

---
+++ R/CrossingTools-package.R
@@ -0,0 +1,8 @@
+#' CrossingTools: Mate allocation and cross evaluation helpers
+#'
+#' Internal package-level documentation stub.
+#'
+#' @keywords internal
+#' @useDynLib CrossingTools, .registration = TRUE
+#' @importFrom Rcpp evalCpp
+"_PACKAGE"
